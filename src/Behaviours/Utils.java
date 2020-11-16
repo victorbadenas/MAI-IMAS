1ad7b6fef216d5b1cfe0c6932d96e7a95ab6a4f1
Albert Ferrando Garrido <albert.ferrando@students.salle.url.edu>
2020-11-16 20:09:38 +0100

    Commenting functions

--- src/Behaviours/Utils.java
+++ src/Behaviours/Utils.java
@@ -10,6 +10,13 @@ import java.io.File;
 public class Utils {
     private static final String log = "[%s] %s";
 
+    /**
+     * Send message to Agent.
+     * @param agent Agent that sends the message
+     * @param type Type of the message (Should be an ACLMessage constant)
+     * @param receiver The receiver of the message. To get the name of an agent use getLocalName() function
+     * @param content Content of the message
+     */
     public static void sendMessage(Agent agent, int type, String receiver, String content) {
         ACLMessage msg = new ACLMessage(type);
         msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
@@ -19,6 +26,13 @@ public class Utils {
         agent.send(msg);
     }
 
+    /**
+     * Same as sendMessage function but without receiver, because the receiver will be the Agent that has sent the message.
+     * @param agent Agent that sends the message
+     * @param msg Message to be responded
+     * @param type Type of the message (Should be an ACLMessage constant)
+     * @param content Content of the message
+     */
     public static void sendReply(Agent agent, ACLMessage msg, int type, String content) {
         ACLMessage reply = msg.createReply();
         reply.setPerformative(type);
@@ -26,14 +40,30 @@ public class Utils {
         agent.send(reply);
     }
 
+    /**
+     * Receive a message
+     * @param a Agent that receives the message
+     * @return The message received by a
+     */
     public static ACLMessage receiveMessage (Agent a) {
         return a.blockingReceive();
     }
 
+    /**
+     * Prints a log
+     * @param a Agent that prints the log
+     * @param message Message of the log
+     */
     public static void log (Agent a, String message) {
         System.out.println(String.format(log, a.getLocalName(), message));
     }
 
+    /**
+     * Check if a human petition is valid or not
+     * @param a Agent that wants to check a petition
+     * @param petition Petition to validate
+     * @return True if valid, false if not valid
+     */
     public static boolean validPetition (Agent a, String petition) {
         String filename = petition.substring(2);
         File checkFile = new File("files/" + filename);
