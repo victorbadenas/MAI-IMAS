13a23f19e25f70bf2c746a1eccf4b5681c8d9733
Victor Badenas <victor.badenas@gmail.com>
2020-11-17 16:31:41 +0100

    added better logging to Fuzzy Agent

--- src/Agents/FuzzyAgent.java
+++ src/Agents/FuzzyAgent.java
@@ -1,6 +1,7 @@
 package Agents;
 import Behaviours.FIPAReciever;
 import Utils.InferenceResult;
+import Utils.Utils;
 import jade.core.*;
 import net.sourceforge.jFuzzyLogic.FIS;
 
@@ -13,40 +14,61 @@ public class FuzzyAgent extends Agent {
         Object[] args = getArguments();
         String fcl = args[0].toString();
         this.fisFileName = "files/" + fcl + ".fcl";
-        System.out.println("Initializing User Agent with name: " + getAID().getLocalName());
-
-        System.out.println("Loading FIS file from: " + this.fisFileName);
-        this.fis = FIS.load(this.fisFileName);
-        System.out.println("FIS file loaded!");
-
+        Utils.log(this, "Initializing User Agent with name: " + getAID().getLocalName());
+        this.loadFis();
         this.addBehaviour(new FIPAReciever(this));
+    }
+
+    private void loadFis(){
+        Utils.log(this, "Loading FIS file from: " + this.fisFileName);
+        try {
+            this.fis = FIS.load(this.fisFileName);
+            Utils.log(this, "FIS file loaded!");
+        } catch (Exception e) {
+            Utils.error(this, "FIS loading FAILED");
+            Utils.error(this, e.getMessage());
+        }
     }
 
     @Override
     protected void takeDown() {
+        Utils.log(this, "Killing fuzzy agent with FIS file: " + this.fisFileName);
         super.takeDown();
         fis = null;
         fisFileName = null;
         System.gc();
+        Utils.log(this, "Agent killed.");
     }
 
     public double[] parseDoubleMessage(String message, String separator){
-        String[] parts = message.split(separator);
-        double[] doubleParts = new double[parts.length];
-        for(int i=0; i<parts.length; i++) {
-            doubleParts[i] = Double.parseDouble(parts[i]);
+        String[] parts;
+        double[] doubleParts = null;
+        try {
+            parts = message.split(separator);
+            doubleParts = new double[parts.length];
+            for(int i=0; i<parts.length; i++) {
+                doubleParts[i] = Double.parseDouble(parts[i]);
+            }
+        } catch (Exception e) {
+            Utils.error(this, String.format("Value parsing failed. Expected comma separated values. Recieved %s", message));
+            Utils.error(this, e.getMessage());
         }
         return doubleParts;
     }
 
     public InferenceResult inferFCL(double humidity, double temperature) {
+        Utils.log(this, String.format("Infering from data: %f, %f", humidity, temperature));
+
         try{
             this.fis.setVariable("humidity", humidity);
             this.fis.setVariable("temperature", temperature);
             this.fis.evaluate();
-            return new InferenceResult(true, this.fis.getVariable("duration_period").getLatestDefuzzifiedValue());
+            double numericalResult = this.fis.getVariable("duration_period").getLatestDefuzzifiedValue();
+            Utils.log(this, String.format("Infered correctly. Result: %f", numericalResult));
+            return new InferenceResult(true, numericalResult);
         } catch (Exception e) {
-            System.out.println(String.format("ERROR: Error found when infering %d and %d", humidity, temperature));
+            Utils.error(this, String.format("ERROR: Error found when infering %f and %f", humidity, temperature));
+            Utils.error(this, e.getMessage());
             return new InferenceResult(false, this.fis.getVariable("duration_period").getLatestDefuzzifiedValue());
         }
     }
